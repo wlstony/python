da6f7b9a143c86fa75f179f1797074930d6c5e09
linsheng.wu <linsheng.wu@xianghuanji.com>
2018-07-29 17:10:23 +0800

    开发发布会相关接口

--- guest/settings.py
+++ guest/settings.py
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -77,18 +77,18 @@ WSGI_APPLICATION = 'guest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # "ENGINE": "django.db.backends.mysql",
-        # "HOST": "127.0.0.1",
-        # "PORT": "3306",
-        # "NAME": "guest",
-        # "USER": "root",
-        # "PASSWORD": "",
-        # "OPTIONS": {
-        #     "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "NAME": "guest",
+        "USER": "root",
+        "PASSWORD": "",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'"
 
-        # }
+        }
     }
 }
 
---
+++ sign/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from sign import views_if
+
+urlpatterns = [
+    #sign system interface
+    #ex: /api/add_event
+    url(r'^add_event/',  views_if.add_event, name="add_event"),
+
+    #ex: /api/user_sign
+    url(r'^user_sign/',  views_if.user_sign, name="user_sign"),
+
+    #ex: /api/get_event_list/
+    url(r'^get_event_list/', views_if.get_event_list, name="get_event_list"),
+]
